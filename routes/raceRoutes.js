8e181a94216698af1b75948215d0a7a2e4a5aebd
KhalaidW <walidkhalaid@gmail.com>
2026-02-10 15:57:04 -0500

    patch command works

--- routes/raceRoutes.js
+++ routes/raceRoutes.js
@@ -1,22 +1,53 @@
 // Imports
 import express from 'express';
 import db from "../db/conn.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
 // Create
-router.route("/").post(async (req, res) => {
-    // Specify/Choose Collection
-    let collection = await db.collection('races');
+router
+    .route("/")
+    .post(async (req, res) => {
+        // Specify/Choose Collection
+        let collection = await db.collection('races');
 
-    // Perform Action
-    let result = await collection.insertOne(req.body);
+        // Perform Action
+        let result = await collection.insertOne(req.body);
 
-    // Return results
-    res.json(result);
-});
+        // Return results
+        res.json(result);
+    })
+    .get(async (req, res) => {
+        // Specify Collection
+        let collection = db.collection('races');
+
+        // Perform Action
+        let results = await collection.find({}).toArray();
+
+        // Return the Results
+        res.json(results);
+    })
 
 // Read - Show All
+router
+    .route("/:id")
+    .patch(async (req, res) =>{
+        // Create filterable object
+        let query = { _id: new ObjectId(req.params.id )};
+
+        // create update object
+        let update = { $set: req.body };
+
+        // Specify Collection
+        let collection = db.collection("races");
+
+        // Perform Action
+        let results = await collection.updateOne(query, update);
+
+        // Return Results
+        res.json(results);
+    })
 
 // Update
 
